0dff7e082765366ed6e26f347b6861dc36ceddfc
dujf <king54732@163.com>
2019-04-10 14:52:46 +0800

    feat: add jwts

---
+++ jwt-security/src/test/java/org/hippo/jwts/JwtsApplicationTests.java
@@ -0,0 +1,16 @@
+package org.hippo.jwts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JwtsApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
